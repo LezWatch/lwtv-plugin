0afa8e658c195a764d20d564b2e30c340eff10a8
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2016-04-16 17:42:07 -0700

    * Add term meta for icon * Change meta to dropdown so you can’t screw up * Show icon on edit pages * CSS to pretty

--- cmb2.php
+++ cmb2.php
@@ -47,7 +47,7 @@ function cmb2_get_post_options( $query_args ) {
  */
 add_action( 'admin_enqueue_scripts', 'cmb2_lez_scripts', 10 );
 function cmb2_lez_scripts( $hook ) {
-	if ( $hook == 'post.php' || $hook == 'post-new.php' || $hook == 'page-new.php' || $hook == 'page.php' ) {
+	if ( $hook == 'post.php' || $hook == 'post-new.php' || $hook == 'edit-tags.php' || $hook == 'term.php' || $hook == 'page-new.php' || $hook == 'page.php' ) {
 		wp_register_style( 'cmb-styles', plugins_url('/cmb2.css', __FILE__ ) );
 		wp_enqueue_style( 'cmb-styles' );
 	}
@@ -59,6 +59,12 @@ function cmb2_lez_scripts( $hook ) {
 add_action( 'cmb2_admin_init', 'lezwatch_register_taxonomy_metabox' );
 function lezwatch_register_taxonomy_metabox() {
 	$prefix = 'lez_termsmeta_';
+
+	$icon_array = array();
+	foreach (glob( get_stylesheet_directory().'/images/symbolicons/*.svg' ) as $file) {
+		$icon_array[ basename($file, '.svg') ] = basename($file, '.svg');
+	}
+
 	/**
 	 * Metabox to add fields to categories and tags
 	 */
@@ -71,9 +77,50 @@ function lezwatch_register_taxonomy_metabox() {
 	) );
 
 	$cmb_term->add_field( array(
-		'name' => __( 'Icon', 'cmb2' ),
-		'desc' => __( 'Name of the image you want to use (i.e. carrot.svg)', 'cmb2' ),
-		'id'   => $prefix . 'icon',
-		'type' => 'text_small',
+		'name'			=> __( 'Icon', 'cmb2' ),
+		'desc'			=> __( 'Name of the image you want to use', 'cmb2' ),
+		'id'			=> $prefix . 'icon',
+		//'type'			=> 'text_medium',
+	    'type'             => 'select',
+	    'show_option_none' => true,
+	    'default'          => 'custom',
+	    'options'          => $icon_array,
+		'before_field'	=> 'lez_before_field_icon',
 	) );
+}
+
+/**
+ * Show an icon if that exists
+ *
+ * @param  array             $field_args Array of field parameters
+ * @param  CMB2_Field object $field      Field object
+ */
+function lez_before_field_icon( $field_args, $field ) {
+	$icon = $field->value;
+	$iconpath = get_stylesheet_directory().'/images/symbolicons/'.$icon.'.svg';
+	if ( !empty($icon) || file_exists( $iconpath ) ) {
+		echo '<span role="img" class="cmb2-icon">'.file_get_contents(get_stylesheet_directory_uri().'/images/symbolicons/'.$icon.'.svg').'</span>';
+	}
+}
+
+add_filter( "manage_edit-lez_cliches_columns", "lez_terms_column_header_function" );
+add_action( "manage_lez_cliches_custom_column",  "lez_terms_populate_rows_function", 10, 3  );
+add_filter( "manage_edit-lez_chartags_columns", "lez_terms_column_header_function" );
+add_action( "manage_lez_chartags_custom_column",  "lez_terms_populate_rows_function", 10, 3  );
+
+
+function lez_terms_column_header_function($columns){
+    $columns['icon'] = 'Icon';
+    return $columns;
+}
+
+function lez_terms_populate_rows_function($value, $content, $term_id){
+	$icon = get_term_meta( $term_id, 'lez_termsmeta_icon', true );
+	$iconpath = get_stylesheet_directory().'/images/symbolicons/'.$icon.'.svg';
+	if ( empty($icon) || !file_exists( $iconpath ) ) {
+		$content = 'N/A';
+	} else {
+		$content = '<span role="img" class="cmb2-icon">'.file_get_contents($iconpath).'</span>';
+	}
+    return $content;
 }
