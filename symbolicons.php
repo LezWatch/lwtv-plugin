a6026dc721f1d652fefb8ef2d6b4b86c0666b0b5
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2016-04-16 18:34:49 -0700

    * Disable font

--- symbolicons.php
+++ symbolicons.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: Symbolicons Bootstrap
 Plugin URI:  https://fortawesome.com/sets/symbolicons-block
-Description: Boostrap file to load the symbolicons. 
+Description: Boostrap file to load the symbolicons.
 Version: 1.0
 Author: Mika Epstein
 */
 
-add_action( 'wp_enqueue_scripts', 'symbolicons_fonts' );     // Front end
-add_action( 'admin_enqueue_scripts', 'symbolicons_fonts' );  // Back end
+//add_action( 'wp_enqueue_scripts', 'symbolicons_fonts' );     // Front end
+//add_action( 'admin_enqueue_scripts', 'symbolicons_fonts' );  // Back end
 function symbolicons_fonts() {
     wp_enqueue_style( 'symbolicons', plugins_url( 'ss-symbolicons-block/webfonts/ss-symbolicons-block.css' , __FILE__ ) );
 }
