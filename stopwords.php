1bd20bb0d1420710a06670b394af8cbb768d7584
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2016-09-18 11:16:20 -0700

    Don’t run stop words on quick edit.

--- stopwords.php
+++ stopwords.php
@@ -114,27 +114,30 @@ class Stopwords_Admin {
 
 global $typenow;
 
-// when editing pages, $typenow isn't set until later!
-if (empty($typenow)) {
-    // try to pick it up from the query string
-    if (!empty($_GET['post'])) {
-        $post = get_post($_GET['post']);
-        $typenow = $post->post_type;
-    }
-    // try to pick it up from the query string
-    elseif ( !empty($_GET['post_type']) ) {
-	    $typenow = $_GET['post_type'];
-    }
-    // try to pick it up from the quick edit AJAX post
-    elseif (!empty($_POST['post_ID'])) {
-        $post = get_post($_POST['post_ID']);
-        $typenow = $post->post_type;
-    }
-    else {
-	    $typenow = 'fuck';
-    }
-}
+if ( 'all' !== $_POST['post_status'] ) ) {
 
-if ( $typenow !== ( 'post_type_shows' || 'post_type_characters' ) && ( 'all' !== $_POST['post_status'] ) ) {
-	new Stopwords_Admin;
+	// when editing pages, $typenow isn't set until later!
+	if (empty($typenow)) {
+	    // try to pick it up from the query string
+	    if (!empty($_GET['post'])) {
+	        $post = get_post($_GET['post']);
+	        $typenow = $post->post_type;
+	    }
+	    // try to pick it up from the query string
+	    elseif ( !empty($_GET['post_type']) ) {
+		    $typenow = $_GET['post_type'];
+	    }
+	    // try to pick it up from the quick edit AJAX post
+	    elseif (!empty($_POST['post_ID'])) {
+	        $post = get_post($_POST['post_ID']);
+	        $typenow = $post->post_type;
+	    }
+	    else {
+		    $typenow = 'fuck';
+	    }
+	}
+
+	if ( $typenow !== ( 'post_type_shows' || 'post_type_characters' ) ) {
+		new Stopwords_Admin;
+	}
 }
