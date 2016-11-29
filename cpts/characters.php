edf86a712be6774bfe611d6769fe52b129bfe68c
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2016-11-28 19:32:29 -0800

    alt_keyword - doesn’t work

--- cpts/characters.php
+++ cpts/characters.php
@@ -619,3 +619,42 @@ function lez_characters_quick_edit_link($actions, $post) {
 	$actions['inline hide-if-no-js'] .= '</a>';
 	return $actions;
 }
+
+
+/*
+ * Extra Meta Variables for Yoast and Characters
+ *
+ */
+
+// List of actors who played a character, for use on character pages
+function lez_retrieve_actors_replacement( ) {
+	if ( !is_array (get_post_meta( get_the_ID(), 'lezchars_actor', true)) ) {
+		$actors = array( get_post_meta( get_the_ID(), 'lezchars_actor', true) );
+	} else {
+		$actors = get_post_meta( get_the_ID(), 'lezchars_actor', true);
+	}
+	return implode(", ", $actors);
+}
+
+// List of shows featuring a character, for use on character pages
+function lez_retrieve_shows_replacement( ) {
+	if ( !is_array (get_post_meta( get_the_ID(), 'lezchars_show', true)) ) {
+		$shows_ids = array( get_post_meta( get_the_ID(), 'lezchars_show', true) );
+	} else {
+		$shows_ids = get_post_meta( get_the_ID(), 'lezchars_show', true);
+	}
+
+	$shows_titles = array();
+	foreach ( $shows_ids as $show ) {
+		$post_object = get_post( $show );
+		array_push( $shows_titles, '"'. $post_object->post_title .'"' );
+	}
+	return implode(", ", $shows_titles);
+}
+
+// The actual replacement function
+function lez_chars_register_yoast_extra_replacements() {
+	wpseo_register_var_replacement( '%%actors%%', 'lez_retrieve_actors_replacement', 'basic', 'A list of actors who played the character, separated by commas.' );
+	wpseo_register_var_replacement( '%%shows%%', 'lez_retrieve_shows_replacement', 'basic', 'A list of shows the character was on, separated by commas.' );
+}
+add_action( 'wpseo_register_extra_replacements', 'lez_chars_register_yoast_extra_replacements' );
