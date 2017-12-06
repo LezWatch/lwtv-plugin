d01b4f908af2565dcd593264e72b3eaae0015bc6
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2017-12-06 09:12:14 -0800

    Fixing OTD

--- rest-api/of-the-day.php
+++ rest-api/of-the-day.php
@@ -116,18 +116,19 @@ class LWTV_OTD_JSON {
 				$shows_i_am_on    = ( empty( $show_title ) )? ' None' : implode( ', ', $show_title );
 				$dead_or_alive    = ( has_term( 'dead', 'lez_cliches' , $post_id ) )? 'dead' : 'alive';
 				$of_the_day_array = array(
-					'name'   => get_the_title( $id ),
+					'id'     => $post_id,
+					'name'   => get_the_title( $post_id ),
 					'status' => $dead_or_alive,
 					'shows'  => $shows_i_am_on,
-					'url'    => get_the_permalink( $id ),
+					'url'    => get_the_permalink( $post_id ),
 				);
 				break;
 			case 'show':
 				$show_we_love     = ( get_post_meta( $post_id, 'lezshows_worthit_show_we_love', true ) )? 'yes' : 'no';
 				$of_the_day_array = array(
-					'name'  => get_the_title( $id ),
+					'name'  => get_the_title( $post_id ),
 					'loved' => $show_we_love,
-					'url'   => get_permalink( $id ),
+					'url'   => get_permalink( $post_id ),
 				);
 				break;
 		}
