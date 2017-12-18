077526050938d1aab6dba55253d287d256b5af7f
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2017-12-18 15:57:58 -0800

    First pass at new features.
    * Allow images for tweet of the day
    * New options for BYQ of the day as a tweet (maybe)

--- rest-api/of-the-day.php
+++ rest-api/of-the-day.php
@@ -57,82 +57,91 @@ class LWTV_OTD_JSON {
 	public static function of_the_day( $type = 'unknown' ) {
 
 		// Valid types of 'of the day':
-		$valid_types = array( 'character', 'show' );
+		$valid_types = array( 'character', 'show', 'death' );
 
 		// If there's no known type, we'll assume character
 		if ( $type == 'unknown' || !in_array( $type, $valid_types ) ) {
 			$type = 'character';
 		}
 
-		// Grab the options
-		$default = array (
-			'character' => array( 
-				'time'  => strtotime( 'midnight tomorrow' ),
-				'post'  => 'none',
-			),
-			'show'      =>  array( 
-				'time'  => strtotime( 'midnight tomorrow' ),
-				'post'  => 'none',
-			),
-		);
-		$options = get_option( 'lwtv_otd', $default );
+		$of_the_day_array = array();
 
-		// If there's no ID or the timestamp has past, we need a new ID
-		if ( $options[ $type ][ 'post' ] == 'none' || time() >= $options[ $type ][ 'time' ] ) {
-			add_filter( 'facetwp_is_main_query', function( $is_main_query, $query ) { return false; }, 10, 2 );
-			// Grab a random post
-			$args = array( 
-				'post_type'      => 'post_type_' . $type . 's',
-				'orderby'        => 'rand', 
-				'posts_per_page' =>'1',
+		// Death of the day is calculated differently
+		if ( $type == 'death' ) {
+			$of_the_day_array = LWTV_BYQ_JSON::on_this_day( date('m-d'), 'tweet' );
+		} else {
+			// Grab the options
+			$default = array (
+				'character' => array( 
+					'time'  => strtotime( 'midnight tomorrow' ),
+					'post'  => 'none',
+				),
+				'show'      =>  array( 
+					'time'  => strtotime( 'midnight tomorrow' ),
+					'post'  => 'none',
+				),
 			);
-			$post = new WP_Query( $args );
-
-			// Do the needful
-			while ( $post->have_posts() ) {
-				$post->the_post();
-				$id = get_the_ID();
-			}
-			wp_reset_postdata();
-			
-			// Update the options
-			$options[ $type ][ 'post' ] = $id;
-			$options[ $type ][ 'time' ] = strtotime( 'midnight tomorrow' );
-			update_option( 'lwtv_otd', $options );
-		}
-
-		$post_id = $options[ $type ][ 'post' ];
-
-		// Generate Array
-		switch( $type ) {
-			case 'character':
-				$all_shows        = lwtv_yikes_chardata( $post_id, 'shows' );
-				if ( $all_shows !== '' ) {
-					$show_title = array();
-					foreach ( $all_shows as $each_show ) {
-						array_push( $show_title, get_the_title( $each_show['show'] ) );
-					}
+			$options = get_option( 'lwtv_otd', $default );
+	
+			// If there's no ID or the timestamp has past, we need a new ID
+			if ( $options[ $type ][ 'post' ] == 'none' || time() >= $options[ $type ][ 'time' ] ) {
+				add_filter( 'facetwp_is_main_query', function( $is_main_query, $query ) { return false; }, 10, 2 );
+				// Grab a random post
+				$args = array( 
+					'post_type'      => 'post_type_' . $type . 's',
+					'orderby'        => 'rand', 
+					'posts_per_page' =>'1',
+				);
+				$post = new WP_Query( $args );
+	
+				// Do the needful
+				while ( $post->have_posts() ) {
+					$post->the_post();
+					$id = get_the_ID();
 				}
-				$shows_i_am_on    = ( empty( $show_title ) )? ' None' : implode( ', ', $show_title );
-				$dead_or_alive    = ( has_term( 'dead', 'lez_cliches' , $post_id ) )? 'dead' : 'alive';
-				$of_the_day_array = array(
-					'id'     => $post_id,
-					'name'   => get_the_title( $post_id ),
-					'status' => $dead_or_alive,
-					'shows'  => $shows_i_am_on,
-					'url'    => get_the_permalink( $post_id ),
-				);
-				break;
-			case 'show':
-				$show_we_love     = ( get_post_meta( $post_id, 'lezshows_worthit_show_we_love', true ) )? 'yes' : 'no';
-				$of_the_day_array = array(
-					'name'  => get_the_title( $post_id ),
-					'loved' => $show_we_love,
-					'url'   => get_permalink( $post_id ),
-				);
-				break;
+				wp_reset_postdata();
+				
+				// Update the options
+				$options[ $type ][ 'post' ] = $id;
+				$options[ $type ][ 'time' ] = strtotime( 'midnight tomorrow' );
+				update_option( 'lwtv_otd', $options );
+			}
+	
+			$post_id = $options[ $type ][ 'post' ];
+			$image   = get_site_icon_url();
+			if ( has_post_thumbnail( $post_id ) ) {
+				$image = get_the_post_thumbnail_url( $post_id, 'full' ) ;
+			}
+	
+			// Base Array:
+			$of_the_day_array = array(
+				'id'     => $post_id,
+				'name'   => get_the_title( $post_id ),
+				'url'    => get_the_permalink( $post_id ),
+				'image'  => $image,
+			);
+	
+			// Add custom array items based on type
+			switch( $type ) {
+				case 'character':
+					$all_shows        = lwtv_yikes_chardata( $post_id, 'shows' );
+					if ( $all_shows !== '' ) {
+						$show_title = array();
+						foreach ( $all_shows as $each_show ) {
+							array_push( $show_title, get_the_title( $each_show['show'] ) );
+						}
+					}
+					$of_the_day_array['status'] = ( has_term( 'dead', 'lez_cliches' , $post_id ) )? 'dead' : 'alive';
+					$of_the_day_array['shows']  = ( empty( $show_title ) )? ' None' : implode( ', ', $show_title );
+					break;
+				case 'show':
+					$of_the_day_array['loved'] = ( get_post_meta( $post_id, 'lezshows_worthit_show_we_love', true ) )? 'yes' : 'no';
+					break;
+			}
+
 		}
 
+		// Return array
 		return $of_the_day_array;
 	}
 
