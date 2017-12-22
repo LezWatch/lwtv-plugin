9815c2560fc37dda0ed515e9b1bcb4a3c544a9db
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2017-12-21 16:40:27 -0800

    WP CLI code to update actors once we have a list.

--- wp-cli.php
+++ wp-cli.php
@@ -69,16 +69,37 @@ class WP_CLI_LWTV_ShowCalc_Command extends WP_CLI_Command {
 
 				$number_chars = count( lwtv_yikes_actordata( $post_id, 'characters' ) );
 				$number_dead  = count( lwtv_yikes_actordata( $post_id, 'dead' ) );
+				$is_queer     = ( LWTV_Loops::is_actor_queer( $post_id ) == 'yes' )? true : false;
 				update_post_meta( $post_id, 'lezactors_char_count', $number_chars );
 				update_post_meta( $post_id, 'lezactors_dead_count', $number_dead );
+				update_post_meta( $post_id, 'lezactors_queer', $is_queer );
+				
+				$output  = $number_chars . ' chars, ' . $number_dead . ' dead';
+				$output .= ( $is_queer )? ', is queer' : '';
+
+				// If we're not queer, let's check if they need to be updated to have SOME data ...
+				if ( !$is_queer ) {
+					$gender_terms    = get_the_terms( $post_id, 'lez_actor_gender', true );
+					$sexuality_terms = get_the_terms( $post_id, 'lez_actor_sexuality', true );
+
+					// If no gender is set OR there's an error...
+					if ( !$gender_terms || is_wp_error( $gender_terms ) ) 
+						wp_set_object_terms( $post_id, 'cisgender', 'lez_actor_gender', false );
+						$output .= ', added cisgender';
+					// Ditto Gender
+					if ( !$sexuality_terms || is_wp_error( $sexuality_terms ) ) 
+						wp_set_object_terms( $post_id, 'heterosexual', 'lez_actor_sexuality', false );
+						$output .= ', added heterosexual';
+				}
+
 			} else {
-				WP_CLI::error( 'You can only calculate the score on show pages.' );
+				WP_CLI::error( 'You can only update actor meta on actor pages.' );
 			}
 		} else {
-			WP_CLI::error( 'You can only calculate the score on show pages.' );
+			WP_CLI::error( 'You can only update actor meta on actor pages.' );
 		}
 		
-		WP_CLI::success( 'Meta data updated for ' . get_the_title( $post_id ) . ': ' . $number_chars . ' chars, ' . $number_dead . ' dead' );
+		WP_CLI::success( 'Meta data updated for ' . get_the_title( $post_id ) . ': ' . $output );
 	}
 
 }
