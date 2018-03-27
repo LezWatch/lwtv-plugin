181451ed4289d1d45081a157b2ffd6745f11ace7
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2018-03-26 17:59:35 -0700

    Keeping the mail filters handy

--- shortcodes.php
+++ shortcodes.php
@@ -134,9 +134,10 @@ class LWTV_Shortcodes {
 			'trigger' => 'none',
 		), $atts );
 
-		$queer   = ( !is_numeric( $attributes['queer'] ) || ( $attributes['queer'] < 1 || $attributes['queer'] > 5 ) )? $attributes['queer'] : 0;
+		$queer = (int) $attributes['queer'];
+		$queer = ( $queer < 1 || $queer > 5 )? 0 : $queer;
 
-		$worth   = ( in_array( $attributes['worth'], array( 'yes', 'no', 'meh' ) ) )? $attributes['worth'] : 'meh';
+		$worth = ( in_array( $attributes['worth'], array( 'yes', 'no', 'meh' ) ) )? $attributes['worth'] : 'meh';
 		switch ( $worth ) {
 			case 'yes':
 				$worth_icon = 'thumbs-up';
