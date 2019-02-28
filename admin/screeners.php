5bffa0dbf99bb52ee6728a8ffb98c05275b0abad
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2019-02-28 12:31:33 -0800

    AWS
    Update screeners to draw from AWS now instead of DHO, due to shenanigans 
    and stability.

--- admin/screeners.php
+++ admin/screeners.php
@@ -31,7 +31,8 @@ class LWTV_Screeners {
 	 */
 	public static function settings_page() {
 
-		$active_tab = isset( $_GET['tab'] ) ? $_GET['tab'] : 'intro'; // WPCS: CSRF ok.
+		// phpcs:ignore WordPress.Security.NonceVerification
+		$active_tab = isset( $_GET['tab'] ) ? $_GET['tab'] : 'intro';
 
 		// For what it's worth, I would never do this anywhere else, but I'm lazy
 		echo '<style>.collapse { display: none; }.collapse.show { display: block; }</style>';
@@ -124,21 +125,27 @@ class LWTV_Screeners {
 
 		// Check for the AWS functions
 		if ( ! function_exists( 'Aws\constantly' ) ) {
-			echo '<p>Cannot reach DreamObjects at this time. Yell at Mika.</p>';
+			echo '<p>Cannot reach Amazon at this time.</p>';
 		} else {
-			// Establish connection with DreamObjects with an S3 client.
-			$client = new Aws\S3\S3Client([
-				'version'     => '2006-03-01',
-				'region'      => 'us-east-1',
-				'endpoint'    => 'https://objects-us-east-1.dream.io',
-				'credentials' => [
-					'key'    => DHO_ACCESS_KEY_ID,
-					'secret' => DHO_SECRET_ACCESS_KEY,
-				],
-			]);
+			// Establish connection with AWS S3
+			$client = new Aws\S3\S3Client(
+				[
+					'version'     => '2006-03-01',
+					'region'      => 'us-west-2',
+					'credentials' => [
+						'key'    => AMAZON_PRODUCT_API_KEY,
+						'secret' => AMAZON_PRODUCT_API_SECRET,
+					],
+				]
+			);
 
-			$bucket  = 'lezpress-screeners';
-			$objects = $client->listObjectsV2( [ 'Bucket' => $bucket ] );
+			$bucket  = 'lezwatch';
+			$objects = $client->listObjectsV2(
+				[
+					'Bucket' => $bucket,
+					'Prefix' => 'screeners/',
+				]
+			);
 
 			?>
 			<p>Videos you can download and watch and damn the man!</p>
@@ -148,30 +155,18 @@ class LWTV_Screeners {
 
 						echo '<li>';
 
-						if ( '/' === substr( $object['Key'], -1 ) ) {
-							$name = rtrim( $object['Key'], '/' );
-							echo '<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#' . esc_attr( strtolower( $name ) ) . '">' . esc_attr( $name ) . '</button>';
-							echo '<div id="' . esc_attr( strtolower( $name ) ) . '" class="collapse"><ul>';
-
-							$subobjects = $client->listObjectsV2([
-								'Bucket'     => $bucket,
-								'Prefix'     => $object['Key'],
-								'StartAfter' => $object['Key'],
-							]);
-
-							foreach ( $subobjects['Contents'] as $subobject ) {
-								if ( substr( $subobject['Key'], -1 ) !== '/' ) {
-									$plain_url  = $client->getObjectUrl( $bucket, $subobject['Key'] );
-									$cmd        = $client->getCommand('GetObject', [
-										'Bucket' => $bucket,
-										'Key'    => $subobject['Key'],
-									]);
-									$signed_url = $client->createPresignedRequest( $cmd, '+1 hour' );
-									$size       = self::make_readable( $subobject['Size'] );
-									echo '<li>&bull; <a href="' . esc_url( $signed_url->getUri() ) . '" download>' . esc_html( $subobject['Key'] ) . '</a> - ' . esc_html( $size ) . '</li>';
-								}
-							}
-							echo '</ul></div>';
+						if ( '/' !== substr( $object['Key'], -1 ) ) {
+							$plain_url  = $client->getObjectUrl( $bucket, $object['Key'] );
+							$cmd        = $client->getCommand(
+								'GetObject',
+								[
+									'Bucket' => $bucket,
+									'Key'    => $object['Key'],
+								]
+							);
+							$signed_url = $client->createPresignedRequest( $cmd, '+1 hour' );
+							$size       = self::make_readable( $object['Size'] );
+								echo '<li>&bull; <a href="' . esc_url( $signed_url->getUri() ) . '" download>' . esc_html( $object['Key'] ) . '</a> - ' . esc_html( $size ) . '</li>';
 						}
 
 						echo '</li>';
