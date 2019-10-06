d722307e84a9fdfd34a1c1d1d1417e77d02d689b
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2019-10-06 16:42:47 -0700

    Composer/Node
    * Move everything that can be in Node to Node
    * Teach Node to copy files to the right places
    * Make Composer ONLY what is needed to run
    * Install ICS Parser

---
+++ node_scripts/postinstall.js
@@ -0,0 +1,37 @@
+/*
+    This script runs all the various node post install things we need for LTWV.
+
+    1. Downloads FacetWP-CMB2 and CMB2 Grid Code
+    2. Copies other files into place
+ */
+
+const download = require('download-git-repo');
+var copy = require('copy');
+
+// FacetWP + CMB2
+download('WebDevStudios/facetwp-cmb2', 'plugins/facetwp/facetwp-cmb2/', function (err) {
+  if (err) return console.log(err);
+});
+
+// CMB2 Grid Code
+download('origgami/cmb2-grid', 'plugins/cmb2/cmb2-grid/', function (err) {
+  if (err) return console.log(err);
+});
+
+// CMB Field Select2 (Forked and not used.)
+//mustardbees/cmb-field-select2
+
+// Chart.JS
+copy.one( "node_modules/chart.js/dist/Chart.bundle.min.js", "assets/js", {flatten: true}, function(err) {
+  if (err) return console.log(err);
+});
+
+// TableSorter
+copy.one( "node_modules/tablesorter/dist/js/jquery.tablesorter.js", "assets/js", {flatten: true}, function(err) {
+  if (err) return console.log(err);
+});
+
+// TableSorter Bootstrap
+copy.one( "node_modules/tablesorter/dist/css/theme.bootstrap_4.min.css", "assets/css", {flatten: true}, function(err) {
+  if (err) return console.log(err);
+});
