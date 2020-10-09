a163918156c531d5261df2ba6a387591fd01085c
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2020-10-09 16:13:26 -0700

    Algolia Tweaks
    Using show scores to order things. Mostly works (except for the 
    Flash...)

--- plugins/algolia.php
+++ plugins/algolia.php
@@ -10,11 +10,7 @@ class LWTV_Algolia {
 		add_filter( 'algolia_should_index_user', array( $this, 'algolia_never_index' ) );
 		add_filter( 'algolia_should_index_term', array( $this, 'algolia_never_index' ) );
 		add_filter( 'algolia_post_shared_attributes', array( $this, 'algolia_reduce_attributes' ), 10, 2 );
-		add_filter( 'algolia_post_posts_shared_attributes', array( $this, 'algolia_reduce_attributes' ), 10, 2 );
-		add_filter( 'algolia_post_pages_shared_attributes', array( $this, 'algolia_reduce_attributes' ), 10, 2 );
-		add_filter( 'algolia_post_post_type_actors_shared_attributes', array( $this, 'algolia_reduce_attributes' ), 10, 2 );
-		add_filter( 'algolia_post_post_type_shows_shared_attributes', array( $this, 'algolia_reduce_attributes' ), 10, 2 );
-		add_filter( 'algolia_post_post_type_characters_shared_attributes', array( $this, 'algolia_reduce_attributes' ), 10, 2 );
+		add_filter( 'algolia_searchable_post_shared_attributes', array( $this, 'algolia_reduce_attributes' ), 10, 2 );
 	}
 
 	/**
@@ -34,13 +30,28 @@ class LWTV_Algolia {
 	 * @return array               Cleaned array of attributes
 	 */
 	public function algolia_reduce_attributes( array $attributes, WP_Post $post ) {
-		$remove_array = array( 'taxonomies_hierarchical', 'post_excerpt', 'post_modified', 'comment_count', 'menu_order', 'taxonomies', 'post_author', 'post_mime_type' );
 
+		// Remove things we're not using to make it easier.
+		$remove_array = array( 'taxonomies_hierarchical', 'post_excerpt', 'post_modified', 'comment_count', 'menu_order', 'taxonomies', 'post_author', 'post_mime_type' );
 		foreach ( $remove_array as $remove_this ) {
 			if ( isset( $attributes[ $remove_this ] ) ) {
 				unset( $attributes[ $remove_this ] );
 			}
 		}
+
+		// Add Scoring for individual ranking
+		switch ( $post->post_type ) {
+			case 'post_type_shows':
+				$score = round( get_post_meta( $post->ID, 'lezshows_the_score', true ), 2 );
+				break;
+			case 'post_type_characters':
+			case 'post_type_actors':
+				$score = 50;
+				break;
+			default:
+				$score = 0;
+		}
+		$attributes['score'] = ( isset( $score ) ) ? $score : 0;
 
 		return $attributes;
 	}
