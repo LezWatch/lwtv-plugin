962fd944105cf4d99630a13738135720177d2c42
Mika Epstein (Ipstenu) <ipstenu@halfelf.org>
2020-10-15 12:21:43 -0700

    Sanity check
    IF the class exists, we run it. Else no.

--- plugins/_main.php
+++ plugins/_main.php
@@ -5,7 +5,6 @@
  */
 
 // Include the base files
-//require_once 'algolia.php'; // No longer used -- yeah that was fast. Keeping just in case.
 require_once 'cmb2.php';
 require_once 'cache.php';
 require_once 'facetwp.php';
--- plugins/jetpack.php
+++ plugins/jetpack.php
@@ -12,24 +12,27 @@ class LWTV_Jetpack {
 
 	/**
 	 * Jetpack Search Filters
+	 * We want to make sure we get post types in there.
 	 */
 	public function init_jetpack_search_filters() {
-		Jetpack_Search::instance()->set_filters( [
-			'Content Type' => [
-				'type'     => 'post_type',
-				'count'    => 10,
-			],
-			'Categories' => [
-				'type'     => 'taxonomy',
-				'taxonomy' => 'category',
-				'count'    => 10,
-			],
-			'Tags' => [
-				'type'     => 'taxonomy',
-				'taxonomy' => 'post_tag',
-				'count'    => 10,
-			],
-		] );
+		if ( class_exists( 'Jetpack_Search' ) ) {
+			Jetpack_Search::instance()->set_filters( [
+				'Content Type' => [
+					'type'     => 'post_type',
+					'count'    => 10,
+				],
+				'Categories' => [
+					'type'     => 'taxonomy',
+					'taxonomy' => 'category',
+					'count'    => 10,
+				],
+				'Tags' => [
+					'type'     => 'taxonomy',
+					'taxonomy' => 'post_tag',
+					'count'    => 10,
+				],
+			] );
+		}
 	}
 }
 
