8aca39a08247ef6aae8982fec593095daa9e2316
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2021-04-28 10:10:41 -0700

    Gravity Forms

--- plugins/_main.php
+++ plugins/_main.php
@@ -8,5 +8,6 @@
 require_once 'cmb2.php';
 require_once 'cache.php';
 require_once 'facetwp.php';
+require_once 'gravity-forms.php';
 require_once 'imagify.php';
 require_once 'jetpack.php';
---
+++ plugins/gravity-forms.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Gravity Forms for WordPress hooks
+ * @package lwtv-plugin
+ */
+
+class LWTV_Gravity_Forms {
+
+	public function __construct() {
+		// https://docs.gravityforms.com/gform_disable_view_counter/#1-disable-for-all-forms
+		add_filter( 'gform_disable_view_counter', '__return_true' );
+	}
+}
+
+new LWTV_Gravity_Forms();
