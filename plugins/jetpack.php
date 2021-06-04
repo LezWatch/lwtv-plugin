0c4c3f2107bdd3d80c673795d86016d4662a8288
Mika A Epstein <mika.epstein@dreamhost.com>
2021-06-04 15:56:04 -0700

    FFS Jetpack.

--- plugins/jetpack.php
+++ plugins/jetpack.php
@@ -37,3 +37,19 @@ class LWTV_Jetpack {
 }
 
 new LWTV_Jetpack();
+
+/**
+ * Remove Jetpack's External Media feature.
+ */
+add_action(
+    'enqueue_block_editor_assets',
+    function () {
+        $disable_external_media = <<<JS
+document.addEventListener( 'DOMContentLoaded', function() {
+    wp.hooks.removeFilter( 'blocks.registerBlockType', 'external-media/individual-blocks' );
+    wp.hooks.removeFilter( 'editor.MediaUpload', 'external-media/replace-media-upload' );
+} );
+JS;
+        wp_add_inline_script( 'jetpack-blocks-editor', $disable_external_media );
+    }
+);
