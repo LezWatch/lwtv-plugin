19b80064cbecd12fe88987b1f034853b73908dfb
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2023-07-20 13:59:26 -0700

    force int

--- features/grading.php
+++ features/grading.php
@@ -25,25 +25,25 @@ class LWTV_Grading {
 		// External scores
 		$external = get_post_meta( $show_id, 'lezshows_3rd_scores', true );
 		$tmdb     = array(
-			'score' => ( isset( $external['tmdb']['score'] ) ) ? round( $external['tmdb']['score'] ) : 'TBD',
+			'score' => ( isset( $external['tmdb']['score'] ) ) ? round( (int) $external['tmdb']['score'] ) : 'TBD',
 			'url'   => ( isset( $external['tmdb']['url'] ) ) ? $external['tmdb']['url'] : 'https://themoviedb.org',
 		);
 		$tvmaze   = array(
-			'score' => ( isset( $external['tvmaze']['score'] ) ) ? round( $external['tvmaze']['score'] ) : 'TBD',
+			'score' => ( isset( $external['tvmaze']['score'] ) ) ? round( (int) $external['tvmaze']['score'] ) : 'TBD',
 			'url'   => ( isset( $external['tvmaze']['url'] ) ) ? $external['tvmaze']['url'] : 'https://tvmaze.com',
 		);
 /*
 		$tomato   = array(
-			'score' => ( isset( $external['tomatoes']['score'] ) ) ? round( $external['tomatoes']['score'] ) : 'TBD',
+			'score' => ( isset( $external['tomatoes']['score'] ) ) ? round( (int) $external['tomatoes']['score'] ) : 'TBD',
 			'url'   => ( isset( $external['tomatoes']['url'] ) ) ? $external['tomatoes']['url'] : 'https://rottentomates.com',
 		);
 		$tomato_u = array(
-			'score' => ( isset( $external['tomato_u']['score'] ) ) ? round( $external['tomato_u']['score'] ) : 'TBD',
+			'score' => ( isset( $external['tomato_u']['score'] ) ) ? round( (int) $external['tomato_u']['score'] ) : 'TBD',
 			'url'   => ( isset( $external['tomato']['url'] ) ) ? $external['tomato']['url'] : 'https://rottentomates.com',
 		);
 		$fresh    = ( $tomato >= 60 ) ? 'fresh' : 'splat';
 		$imdb     = array(
-			'score' => ( isset( $external['imdb']['score'] ) ) ? round( $external['imdb']['score'] ) : 'TBD',
+			'score' => ( isset( $external['imdb']['score'] ) ) ? round( (int) $external['imdb']['score'] ) : 'TBD',
 			'url'   => ( isset( $external['imdb']['url'] ) ) ? $external['imdb']['url'] : 'https://imdb.com',
 		);
 */
