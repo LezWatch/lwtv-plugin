de94802e6019d3982edd71dafbfbae0b5724d1b9
Mika Ipstenu Epstein <ipstenu@ipstenu.org>
2023-11-01 07:48:53 -0700

    in progress

--- plugins/_main.php
+++ plugins/_main.php
@@ -5,11 +5,25 @@
  */
 
 // Include the base files
-require_once 'cmb2.php';
 require_once 'cache.php';
-require_once 'facetwp.php';
 require_once 'gutenslam.php';
-require_once 'gravity-forms.php';
-require_once 'jetpack.php';
-require_once 'related-posts-by-taxonomy.php';
-require_once 'yoast.php';
+
+// Include conditionally if the parent is active.
+if ( is_plugin_active( 'cmb2/init.php' ) ) {
+	require_once 'cmb2.php';
+}
+if ( is_plugin_active( 'facetwp/index.php' ) ) {
+	require_once 'facetwp.php';
+}
+if ( is_plugin_active( 'gravityforms/gravityforms.php' ) ) {
+	require_once 'gravity-forms.php';
+}
+if ( is_plugin_active( 'jetpack/jetpack.php' ) ) {
+	require_once 'jetpack.php';
+}
+if ( is_plugin_active( 'related-posts-by-taxonomy/related-posts-by-taxonomy.php' ) ) {
+	require_once 'related-posts-by-taxonomy.php';
+}
+if ( is_plugin_active( 'wordpress-seo-premium/wp-seo-premium.php' ) ) {
+	require_once 'yoast.php';
+}
